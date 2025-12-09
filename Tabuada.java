28a1fbc9cb46116ef593202edda38d3713d2efd2
Diego Rodrigues dos Santos <diegoorodrigues006@gmail.com>
2025-12-09 18:56:19 +0000

    usa teclado

---
+++ Tabuada.java
@@ -0,0 +1,38 @@
+/** 
+* Programa Java calcula a tabuada de um determinado número.
+*Ojetivos:
+* - exercício de lógica
+* uso do comando for (repetição);
+*
+*/
+    
+import java.util.scanner;
+
+public class Tabuada {
+
+public static void main(String[] args) {
+Scanner teclado = new Scanner(System.in);
+
+System.out.print("Informe o número para calcular a tabuada: ");
+int numero = teclaco.nextInt();
+calculaTabuada(numero);
+}
+
+/*
+* Calcula da tabuado do número informado como argumento.
+*/
+static void calculaTabuada(int n) {
+    if (n<=0) {
+        System.out.println("Informe um número válido para calcular a tabuada!");
+        return;
+    }
+
+    System.out.println("Calculando tabuadas do " + n + "...");
+
+    for (int multiplicador = 1; multiplicador <= 10; multilplicados++) {
+        int resultado = multiplicador * n;
+        System.out.println(n+" x "+multiplicador+" = " +resultado);
+     }
+    
+    }
+    }
